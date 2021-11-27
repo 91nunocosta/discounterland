6920e5b99b39f9d8d402010c3bcf0cc6cd52ffc2
Nuno Costa <91nunocosta@gmail.com>
2021-11-27 18:58:31 +0000

    test: test unauthenticated create discount

--- tests/app/test_discounts.py
+++ tests/app/test_discounts.py
@@ -13,23 +13,35 @@ def test_add_discount(db, client, token):
 
     expected_response = dict(discount)
 
-    root = client.get()
-    print(root.json)
-
     response = client.post(
         f"/consumers/{consumer_id}/discounts",
         json=discount,
-        headers={"Authorization": token},
+        headers={"authorization": token},
     )
-
-    print(response.json)
 
     assert response.status_code == 201
 
     added_discount = dict(db.discounts.find_one())
 
-    print(added_discount)
-
     assert items_without_meta([added_discount]) == items_without_meta(
         [expected_response]
     )
+
+
+def test_add_discount_for_non_authenticated_user(db, client):
+    db.discounts.drop()
+
+    promotion_id = "61a22c8f43cf71b9933afdd7"
+    consumer_id = "61a22cb797321cee10c8df49"
+
+    discount = {
+        "promotion_id": promotion_id,
+    }
+
+    response = client.post(
+        f"/consumers/{consumer_id}/discounts",
+        json=discount,
+    )
+
+    assert response.status_code == 401
+
