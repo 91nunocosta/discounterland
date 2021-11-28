ee0fdf27a3784156b2fd78ad115487ceb81acdb8
Nuno Costa <91nunocosta@gmail.com>
2021-11-28 01:01:01 +0000

    docs: fix mongo-init.js

--- examples.sh
+++ examples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "\n\n Create discount\n"
+printf "\n\n Create a promotion\n"
 curl --location --request POST 'http://0.0.0.0:5000/brands/61a22c8f43cf71b9933afdd7/promotions' \
 --header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MW51bm9jb3N0YUBnbWFpbC5jb20iLCJpYXQiOjE2MTY2MTY5NjN9.tMQoy_6ROA_sxWR1exWVeRZZZFR4qvMbO2Szos_XIMI' \
 --header 'Content-Type: application/json' \
@@ -15,11 +15,11 @@ curl --location --request POST 'http://0.0.0.0:5000/brands/61a22c8f43cf71b9933af
   "discounts_quantity": 10
 }'
 
-printf "\n\n Create a promotion \n"
-curl --location --request POST 'http://0.0.0.0:5000/consumers/61a22cb797321cee10c8df49/discounts' \
+printf "\n\n Create a discount\n"
+curl --location --request POST 'http://0.0.0.0:5000/consumers/61a2d3be596808c5d69dd11b/discounts' \
 --header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MW51bm9jb3N0YUBnbWFpbC5jb20iLCJpYXQiOjE2MTY2MTY5NjN9.tMQoy_6ROA_sxWR1exWVeRZZZFR4qvMbO2Szos_XIMI' \
 --header 'Content-Type: application/json' \
 --data-raw '{
-  "promotion_id": "/promotions/9e169116-e3de-41cc-952a-149ed1cc4b40"
+  "promotion_id": "61a2d5606ac07b74c824f1a9"
 }'
 
--- mongo-init.js
+++ mongo-init.js
@@ -1,15 +1,35 @@
 db = new Mongo().getDB("eve");
 db.accounts.insert(
   {
+      "_id": ObjectId("61a2d3be596808c5d69dd11b"),
       "username": "91nunocosta@gmail.com",
       "password": "unsercurepassword"
-  });
+  },
+);
 
 db.brands.insert(
     {
         "_id": ObjectId("61a22c8f43cf71b9933afdd7"),
-    });
-db.consumers.insert(
-    {
-        "_id": ObjectId("61a22cb797321cee10c8df49"),
-    });
+    }
+);
+
+db.brand_managers.insert(
+  {
+    "account_id": ObjectId("61a2d3be596808c5d69dd11b"),
+    "brand_id": ObjectId("61a22c8f43cf71b9933afdd7"),
+  }
+);
+
+db.promotions.insert( {
+  "_id": ObjectId("61a2d5606ac07b74c824f1a9"),
+  "brand_id": "61a22c8f43cf71b9933afdd7",
+  "expiration_date": new Date("2100-1-1"),
+  "product": {
+      "name": "Nutella",
+      "images": [
+          "https://images.jumpseller.com/store/hercules-it-llc/10188702/Nutella.jpg"
+      ]
+  },
+  "discounts_quantity": 10,
+});
+
