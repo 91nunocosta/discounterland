c57e89b86526a365e160f39105eb60b9ac16e399
Nuno Costa <91nunocosta@gmail.com>
2021-11-29 15:23:20 +0000

    feat: add logs to consumers auth

--- discounterland/app/consumers.py
+++ discounterland/app/consumers.py
@@ -1,15 +1,22 @@
+import logging
+
 from eve.auth import TokenAuth
 from flask import current_app, request
 from jwt.exceptions import InvalidTokenError
 
 from discounterland.auth.tokens import check_token
 
+LOGGER = logging.getLogger(__name__)
+
 
 class ConsumerJWTTokenAuth(TokenAuth):
     def check_auth(self, token, allowed_roles, resource, method):
         try:
             token_payload = check_token(token)
-        except InvalidTokenError:
+        except InvalidTokenError as err:
+            LOGGER.warning(
+                f"Consumer auth failed. Token {token} is invalid. {type(err).__name__}: {str(err)}"  # noqa
+            )
             return False
 
         username = token_payload["sub"]
@@ -23,6 +30,9 @@ class ConsumerJWTTokenAuth(TokenAuth):
         requested_consumer_id = request.path.rsplit("/")[2]
 
         if auth_consumer_id != requested_consumer_id:
+            LOGGER.warning(
+                f"Consumer auth failed. Consumer {auth_consumer_id}, with username {username} is not authorized to generate discounts for {requested_consumer_id}."  # noqa
+            )
             return False
 
         return True
